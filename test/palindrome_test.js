1e4dd83cd12116e57a7180c35c41e6c2e6b13a12
Zoey Zheng <Zoeyyandi@Zoeys-MacBook-Air.local>
2018-01-11 10:09:10 -0500

    modified test

--- test/palindrome_test.js
+++ test/palindrome_test.js
@@ -2,21 +2,20 @@ const assert = require('chai').assert;
 const isPalindrome = require('../palindrome');
 
 describe('palindrome', () => {
-  it('should return true if a word is a palindrome', () => {
-    let word = 'aabbaa';
-    let result = isPalindrome(word);
-    assert.isTrue(result);
+  let words1 = ['aabbaa', 'a', 'noon'];
+  let words2 = ['abac', 'zoey', 'palindrome'];
+
+  words1.forEach(word => {
+    it(`should return true if ${word} is a palindrome`, () => {
+      let result = isPalindrome(word);
+      assert.isTrue(result);
+    });
   });
 
-  it('should return true if a word is a palindrome', () => {
-    let word = 'a';
-    let result = isPalindrome(word);
-    assert.isTrue(result);
-  });
-
-  it('should return false if a word is NOT a palindrome', () => {
-    let word = 'abac';
-    assert.isFalse(isPalindrome(word));
+  words2.forEach(word => {
+    it(`should return false if ${word} is NOT a palindrome`, () => {
+      assert.isFalse(isPalindrome(word));
+    });
   });
 
   it('should return true if a phrase is a palindrome', () => {
